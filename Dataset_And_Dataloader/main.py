5cb06db25e5df834b4e50d8f2cafc04890902528
mabagit config --global user.name maba <gyj0149@gmail.com>
2023-11-19 20:24:01 +0900

    add dataset

---
+++ Dataset_And_Dataloader/main.py
@@ -0,0 +1,44 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+
+training_data = datasets.FashionMNIST(
+    root="data",  # 학습,테스트 데이터가 저장되는 경로
+    train=True,  # 학습용 또는 테스트용 여부
+    download=True,  # 데이터가 없는 경우에 인터넷에서 다운
+    transform=ToTensor()  # 특징과 정답 변형을 지정
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
